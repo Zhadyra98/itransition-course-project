700570bfa48af7a35f06a1671b6fb5e5adb67601
Zhadyra Oralkhanova <oralkhanova.zhadyra@gmail.com>
2022-08-15 12:18:17 +0400

    feat: create auth middleware and signin/signup endpoints

--- server/index.js
+++ server/index.js
@@ -2,7 +2,9 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
+
 import itemRoutes from './routes/items.js'
+import userRoutes from './routes/users.js'
 
 const app = express();
 
@@ -11,6 +13,7 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}))
 app.use(cors());
 
 app.use('/items', itemRoutes);
+app.use('/user', userRoutes);
 
 const CONNECTION_URL = 'mongodb+srv://Zhadyra:itisNew123!@cluster0.x8rjlug.mongodb.net/?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000;
--- server/routes/items.js
+++ server/routes/items.js
@@ -1,12 +1,13 @@
-import express from 'express'
+import express from 'express';
 import { getItems, createItem, updateItem, deleteItem, likeItem } from '../controllers/items.js';
+import auth from '../middleware/auth.js';
 
 const router = express.Router();
 
 router.get('/', getItems);
-router.post('/', createItem);
-router.patch('/:id', updateItem);
-router.delete('/:id', deleteItem);
-router.patch('/:id/likeItem', likeItem);
+router.post('/', auth, createItem);
+router.patch('/:id', auth, updateItem);
+router.delete('/:id', auth, deleteItem);
+router.patch('/:id/likeItem', auth, likeItem);
 
 export default router;
